ad948f97ad43d6a9af96459d1561e7c64a2f30de
Erla Guðmundsdóttir <erlagudmundsdottir@Erlas-Air.lan>
2023-01-08 21:46:26 +0000

    ready for ios deployment

--- lib/services/audio_session.dart
+++ lib/services/audio_session.dart
@@ -13,6 +13,8 @@ import 'package:Lesaforrit/models/usr.dart' as usr;
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:flutter_cache_manager/file.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:sound_stream/sound_stream.dart';
 import 'package:uuid/uuid.dart';
@@ -23,10 +25,29 @@ import 'package:google_speech/google_speech.dart';
 
 import '../models/ModelProvider.dart';
 
+// Feed your own stream of bytes into the player
+class MyCustomSource extends ja.StreamAudioSource {
+  final List<int> bytes;
+  MyCustomSource(this.bytes);
+
+  @override
+  Future<ja.StreamAudioResponse> request([int start, int end]) async {
+    start ??= 0;
+    end ??= bytes.length;
+    return ja.StreamAudioResponse(
+      sourceLength: bytes.length,
+      contentLength: end - start,
+      offset: start,
+      stream: Stream.value(bytes.sublist(start, end)),
+      contentType: 'audio/x-wav',
+    );
+  }
+}
+
 // Klasi sem inniheldur allar aðferðir og eiginleika sem interacta við Firestore database.
 class AudioSessionService {
   String uid;
-  final _player = ja.AudioPlayer(
+  ja.AudioPlayer _player = ja.AudioPlayer(
     handleInterruptions: false,
     androidApplyAudioAttributes: false,
     handleAudioSessionActivation: false,
@@ -40,6 +61,7 @@ class AudioSessionService {
   bool recognizing = false;
   bool isCancel = false;
   bool isSave = true;
+  AudioSession _audioSession;
 
   Uint8List saveFile(List<Uint8List> contents, sampleRate) {
     // File recordedFile = File(await getFilePath());
@@ -136,8 +158,9 @@ class AudioSessionService {
 
       await _recorder.initialize();
       await _playerStream.initialize();
+      this._audioSession = audioSession;
       // Listen to audio interruptions and pause or duck as appropriate.
-      _handleInterruptions(audioSession);
+      _handleInterruptions(this._audioSession);
 
       // Use another plugin to load audio to play
     });
@@ -154,6 +177,11 @@ class AudioSessionService {
     await _audioStreamSubscription?.cancel();
     await _audioStream?.close();
     recognizing = false;
+    _player = ja.AudioPlayer(
+      handleInterruptions: false,
+      androidApplyAudioAttributes: false,
+      handleAudioSessionActivation: false,
+    );
   }
 
   Future startRecording(resultListener, Function doneListener,
@@ -195,16 +223,34 @@ class AudioSessionService {
   }
 
   Future setPlayerUrl(String url) async {
-    _player.setUrl(url);
+    print("at set online player place");
+    try {
+      await _player.setUrl(url);
+    } catch (err) {
+      print("there was an error setting online player url");
+      print(err);
+    }
   }
 
   Future setPlayerLocalUrl(String url, [int startPosInMiliSec = 0]) async {
-    await _player.setAsset(url,
-        initialPosition: Duration(milliseconds: startPosInMiliSec));
+    print("at Set local url place");
+    try {
+      await _player.setAsset(url,
+          initialPosition: Duration(milliseconds: startPosInMiliSec));
+    } catch (err) {
+      print("there was an error setting local url");
+      print(err);
+    }
   }
 
-  void play() {
-    _player.play();
+  Future play() async {
+    print("player is going to play");
+    try {
+      await _player.play();
+    } catch (err) {
+      print("there was an error playing sound");
+      print(err);
+    }
   }
 
   Future _handleInterruptions(AudioSession audioSession) async {
