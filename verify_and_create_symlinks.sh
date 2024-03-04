cd2602dbbc10908afb1d507432511a768e814b51
alli959 <alexandergudmundsson@gmail.com>
2024-03-04 19:54:41 +0000

    alteration in Android/IOS

--- verify_and_create_symlinks.sh
+++ verify_and_create_symlinks.sh
@@ -13,7 +13,6 @@ if [ ! -d "$SYMLINK_DIR" ]; then
 fi
 
 # Example of creating a symlink for the amplify_auth_cognito plugin
-# You need to replace <path_to_flutter_plugin> with the actual path to the plugin in your Flutter environment
-ln -sf "C:\Users\Notandi\AppData\Local\Pub\Cache\hosted\pub.dev\amplify_auth_cognito-1.4.2" "$SYMLINK_DIR/amplify_auth_cognito"
+ln -sf "$HOME/plugins/amplify_auth_cognito_ios" "$SYMLINK_DIR/amplify_auth_cognito"
 
 echo "Symlinks setup complete."
